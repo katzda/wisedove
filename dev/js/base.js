b60adf27984ca583b15561d1f02d419a3c618962
Daniel Katz <danekkatz@gmail.com>
2018-05-10 15:29:47 +0100

    fixed creen height and article's ordering

--- dev/js/base.js
+++ dev/js/base.js
@@ -1,251 +1,167 @@
-function Page(){
-	Localization.GetLanguage();
-	var pageInitializer = new PageInitializer();
-	/**	provides convenient methods because allows me to register page types in multiple groups **/
-	function PageInitializer(){
-		var types = {};
-		this.CreateType = function(fn,data,selector,multiSelect){
-			return function(){
-				fn(data,selector,multiSelect);
-			};
-		}
-		this.AddPageGroup = function(gname,pageType){
-			if(typeof types[gname] == "undefined"){
-				types[gname] = [];
-			}
-			var length = types[gname].length;
-			types[gname][length] = pageType;
-		}
-		this.ReloadGroup = function(gname){
-			types[gname].forEach(function(elem){
-				elem();
-			});
-		}
-	}
-	/*Help functions which will become the page types*/
-	function WebsiteLocalization(data,selector){
-		var elem = document.querySelector(selector);
-		elem.innerHTML = Localization[data][Localization.Selected];
-	}
-	function LanguageFlags(data,selector){
-		var langFlags = document.querySelectorAll(".webflags label[for='exclChbox'],.webflags span.text");	/*Seach for menu items (sections)*/
-		if(langFlags.length == 0 /*If they've not been created yet*/){
-			langFlags = [];
-			var elem = document.querySelector(selector);
- /*
-				<span class="webflags">
-					<span class="lang"></span>
-					<span class="sel">
-						<select></select>
-						<label></label><input></input>
-						<span class="help">
-							<span class="icon">?</span>
-							<span class="text"></span>
-						</span>
-					</span>
-				</span>
+function WebsiteLocalization(data,selector){
+	var elem = document.querySelector(selector);
+	elem.innerHTML = Localization[data][Localization.Selected];
+}
 
-
-				<span class="webflags">
-					<span class="lang"/>
-					<span class="sel">
-						<select onclick="webpage.SetLanguage(this)">
-							<option value="0"/>
-							<option value="1"/>
-						</select>
-						<label for="exclChbox">Exclusively</label>
-						<input type="checkbox" id="exclChbox" onclick="webpage.ReloadSection();">
-							<span class="help">
-								<span class="icon">?</span>
-								<span class="text">If there is no article in a section in the desired language but exists in others, you will see it in other language according to your choice.</span>
-							</span>
-					</span>
-				</span>
-*/
-			elem.innerHTML = "";
-			var span = document.createElement("span");
-			var span2 = document.createElement("span");
-			var sel = document.createElement("select");
-			sel.setAttribute("onchange","webpage.SetLanguage(this)");
-			span.classList.add("lang");
-			span2.classList.add("sel");
-			span2.appendChild(sel);
-			elem.appendChild(span);
-			elem.appendChild(span2);
-			for(var i=0; i<dir_tree[0].length; i++){
-				var opt = document.createElement("option");
-				opt.setAttribute("value",i);
-				opt.innerHTML = dir_tree[0][i];
-				sel.appendChild(opt);
-			}
-			var lbl = document.createElement("label");
-			var chckbox = document.createElement("input");
-			var spanHelp = document.createElement("span");
-			var spanIcon = document.createElement("span");
-			var spanText = document.createElement("span");
-			span2.appendChild(lbl);
-			span2.appendChild(chckbox);
-			span2.appendChild(spanHelp);
-			spanHelp.classList.add("help");
-			spanIcon.classList.add("icon");
-			spanText.classList.add("text");
-			spanHelp.appendChild(spanIcon);
-			spanHelp.appendChild(spanText);
-			lbl.setAttribute("for","exclChbox");
-			spanIcon.innerHTML = "?";
-			chckbox.setAttribute("type","checkbox");
-			chckbox.setAttribute("id","exclChbox");
-			chckbox.setAttribute("onclick","webpage.ReloadSection();");
-			langFlags[0] = lbl;
-			langFlags[1] = spanText;
-			spanIcon.addEventListener("mouseover",function(obj){
-				spanText.classList.add("tooltip");
-			});			
-			spanIcon.addEventListener("mouseleave",function(obj){
-				spanText.classList.remove("tooltip");
-			});
-		}
-		langFlags[0].innerHTML = Localization.ExclLangChck[Localization.Selected];
-		langFlags[1].innerHTML = Localization.ExclLangHelp[Localization.Selected];
-	}
-	function GetSections(data,selector){
-		var menuItems = document.getElementsByClassName("menuitem");	/*Seach for menu items (sections)*/
-		if(menuItems.length == 0 /*If they've not been created yet*/){
-			menuItems = [];
-			var elem = document.querySelector(selector);
-			elem.innerHTML = "";
-			for(var i=1 ; i<dir_tree.length; i++){
-				var alink = document.createElement("a");
-				alink.classList.add("menuitem");
-				menuItems[menuItems.length] = alink;
-				alink.setAttribute("href","javascript:webpage.LoadSection("+i+");");
-				elem.appendChild(alink);
-			}
-		}
-		for(var i=1 ; i<=menuItems.length; i++){
-			menuItems[i-1].innerHTML = Localization["Section" + i][Localization.Selected];
-		}
-	}
-	this.articles = [];
-	function GetLeads(data,selector){
-		function DoLoad(sectionNo,PageNo,file){
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function(obj) {
-				if (this.readyState == 4 && this.status == 200) {
-					/*prepairing the content*/
-					var article = document.createElement("p");
-					article.innerHTML = this.responseText;
-					if(article.childElementCount == 0){
-						article = [];
-						article[0] = this.responseText;
-					}else{
-						article = article.children;
-					}
-					/*creating other DOM elements - the root article node*/
-					var divArticle = document.createElement("div");
-					divArticle.classList.add("article");
-					var _tmp = obj.srcElement.responseURL.split(document.location.origin+"/")[1].split("/");
-					var ID = _tmp[_tmp.length-1].split(".")[0];
-					var articleId = _tmp[1] + "_" + ID;
-					divArticle.setAttribute("id",articleId);
-					/*creating other DOM elements - the lead*/
-					var divLead = document.createElement("div");
-					divLead.classList.add("lead");
-					var alink = document.createElement("a");
-					var h1 = document.createElement("h1");
-					alink.setAttribute("onclick","javascript:webpage.SelectArticle(this);");
-					alink.setAttribute("href","javascript:void(0);");
-					/*creating other DOM elements - the rest*/
-					var divRest = document.createElement("div");
-					divRest.classList.add("content");
-					divRest.classList.add("invisible");
-					/*appending*/
-					divArticle.appendChild(divLead);
-					divArticle.appendChild(divRest);
-					divLead.appendChild(h1);
-					h1.appendChild(alink);
-					alink.innerHTML = article[0].innerText;
-					while((article.length > 1) && (article[1].tagName != "HR")){
-						divLead.appendChild(article[1]);
-					}
-					/*appending the rest*/			
-					while(article.length > 1){
-						divRest.appendChild(article[1]);
-					}
-					var index = elem.BinaryInsert(divArticle,function(A,B){
-						/*comparator*/
-						var iA = Number.parseInt(A.id.split("_")[1]);
-						var iB = Number.parseInt(B.id.split("_")[1]);
-						if(iA<iB) return -1; else if(iA==iB) return 0; else return 1;
-					});
-					webpage.articles[webpage.articles.length] = articleId;
-				}
-			};
-			xhttp.open("GET", file);
-			xhttp.send();
-		}
-
+function GetSections(data,selector){
+	var menuItems = document.getElementsByClassName("menuitem");	/*Seach for menu items (sections)*/
+	if(menuItems.length == 0 /*If they've not been created yet*/){
+		menuItems = [];
 		var elem = document.querySelector(selector);
 		elem.innerHTML = "";
-		var section = webpage.GetSection();
-		var file;
-		for(var i=0 ; i<dir_tree[section].length; i++){
-			if(dir_tree[section][i][Localization.Selected]/*translation exists*/){
-				file = "./" + section + "/" + (i+1) + "_" + dir_tree[0][Localization.Selected] +".html";
-				DoLoad(section,(i+1),file);
-			}else{
-				if(!document.querySelector("#exclChbox").checked){
-					for(var j=0; j<dir_tree[0].length; j++){
-						if(dir_tree[section][i][j]){
-							if(j != Localization.Selected){
-								file = "./" + section + "/" + (i+1) + "_" + dir_tree[0][j] + ".html";
-								DoLoad(section,(i+1),file);
-								break;
-							}
+		for(var i=1 ; i<dir_tree.length; i++){
+			var alink = document.createElement("a");
+			alink.classList.add("menuitem");
+			menuItems[menuItems.length] = alink;
+			alink.setAttribute("href","javascript:webpage.LoadSection("+i+");");
+			elem.appendChild(alink);
+		}
+	}
+	for(var i=1 ; i<=menuItems.length; i++){
+		menuItems[i-1].innerHTML = Localization["Section" + i][Localization.Selected];
+	}
+}
+
+function GetLeads(data,selector){
+	function DoLoad(sectionNo,PageNo,file){
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function(obj) {
+			if (this.readyState == 4 && this.status == 200) {
+				/*prepairing the content*/
+				var article = document.createElement("p");
+				article.innerHTML = this.responseText;
+				if(article.childElementCount == 0){
+					article = [];
+					article[0] = this.responseText;
+				}else{
+					article = article.children;
+				}
+				/*creating other DOM elements - the root article node*/
+				var divArticle = document.createElement("div");
+				divArticle.classList.add("article");
+				var _tmp = obj.srcElement.responseURL.split(document.location.origin+"/")[1].split("/");
+				var ID = _tmp[_tmp.length-1].split(".")[0];
+				var articleId = _tmp[_tmp.length-2] + "_" + ID;
+				divArticle.setAttribute("id",articleId);
+				/*creating other DOM elements - the lead*/
+				var divLead = document.createElement("div");
+				divLead.classList.add("lead");
+				var alink = document.createElement("a");
+				var h1 = document.createElement("h1");
+				alink.setAttribute("onclick","javascript:webpage.SelectArticle(this);");
+				alink.setAttribute("href","javascript:void(0);");
+				/*creating other DOM elements - the rest*/
+				var divRest = document.createElement("div");
+				divRest.classList.add("content");
+				divRest.classList.add("invisible");
+				/*appending*/
+				divArticle.appendChild(divLead);
+				divArticle.appendChild(divRest);
+				divLead.appendChild(h1);
+				h1.appendChild(alink);
+				alink.innerHTML = article[0].innerText;
+				while((article.length > 1) && (article[1].tagName != "HR")){
+					divLead.appendChild(article[1]);
+				}
+				/*appending the rest*/			
+				while(article.length > 1){
+					divRest.appendChild(article[1]);
+				}
+				var index = elem.BinaryInsert(divArticle,function(A,B){
+					/*comparator*/
+					var iA = Number.parseInt(A.id.split("_")[1]);
+					var iB = Number.parseInt(B.id.split("_")[1]);
+					if(iA<iB) return -1; else if(iA==iB) return 0; else return 1;
+				});
+				console.log(Array.from(elem.children).toString(function(child){
+					return child.id;
+				}));
+			}
+		};
+		xhttp.open("GET", file);
+		xhttp.send();
+	}
+
+	var elem = document.querySelector(selector);
+	elem.innerHTML = "";
+	var section = webpage.GetSection();
+	var file;
+	for(var i=0 ; i<dir_tree[section].length; i++){
+		if(dir_tree[section][i][Localization.Selected]/*translation exists*/){
+			file = "./" + section + "/" + (i+1) + "_" + dir_tree[0][Localization.Selected] +".html";
+			DoLoad(section,(i+1),file);
+		}else{
+			if(!document.querySelector("#exclChbox").checked){
+				for(var j=0; j<dir_tree[0].length; j++){
+					if(dir_tree[section][i][j]){
+						if(j != Localization.Selected){
+							file = "./" + section + "/" + (i+1) + "_" + dir_tree[0][j] + ".html";
+							DoLoad(section,(i+1),file);
+							break;
 						}
 					}
 				}
 			}
 		}
 	}
-	/*Creating the page types using the above functions*/
-	var WebTitle = pageInitializer.CreateType(WebsiteLocalization,"WebTitle","title");					//WebTitle	
-	var WebHeadline = pageInitializer.CreateType(WebsiteLocalization,"WebHeadline",".webheadline");		//WebHeadline	
-	var WebMotto = pageInitializer.CreateType(WebsiteLocalization,"WebMotto",".webmotto");				//WebMotto
-	var Webflags = pageInitializer.CreateType(LanguageFlags,null,".webflags");							//flags
-	var WebSection = pageInitializer.CreateType(GetSections,null,".websection");						//section
-	var WebContent = pageInitializer.CreateType(GetLeads,null,".webcontent");							//content
-	/*Creating the page groups*/
-	pageInitializer.AddPageGroup("wholePage",WebTitle);
-	pageInitializer.AddPageGroup("wholePage",WebHeadline);
-	pageInitializer.AddPageGroup("wholePage",WebMotto);
-	pageInitializer.AddPageGroup("wholePage",Webflags);
-	pageInitializer.AddPageGroup("wholePage",WebSection);
-	pageInitializer.AddPageGroup("wholePage",WebContent);
-	/*User clicks on a section*/
-	pageInitializer.AddPageGroup("sectionChange",WebContent);
+}
 
-	/*Finally the most important public methods*/
-	this.RegisterWebPart = function(idName,selector){
-		webpage[idName] = function(){
-			return document.querySelector(selector);
-		};
+function LanguageFlags(data,selector){
+	var langFlags = document.querySelectorAll(".webflags label[for='exclChbox'],.webflags span.text");	/*Seach for menu items (sections)*/
+	if(langFlags.length == 0 /*If they've not been created yet*/){
+		langFlags = [];
+		var elem = document.querySelector(selector);
+		elem.innerHTML = "";
+		var span = document.createElement("span");
+		var span2 = document.createElement("span");
+		var sel = document.createElement("select");
+		sel.setAttribute("onchange","webpage.SetLanguage(this)");
+		span.classList.add("lang");
+		span2.classList.add("sel");
+		span2.appendChild(sel);
+		elem.appendChild(span);
+		elem.appendChild(span2);
+		for(var i=0; i<dir_tree[0].length; i++){
+			var opt = document.createElement("option");
+			opt.setAttribute("value",i);
+			opt.innerHTML = dir_tree[0][i];
+			sel.appendChild(opt);
+		}
+		var lbl = document.createElement("label");
+		var chckbox = document.createElement("input");
+		var spanHelp = document.createElement("span");
+		var spanIcon = document.createElement("span");
+		var spanText = document.createElement("span");
+		span2.appendChild(lbl);
+		span2.appendChild(chckbox);
+		span2.appendChild(spanHelp);
+		spanHelp.classList.add("help");
+		spanIcon.classList.add("icon");
+		spanText.classList.add("text");
+		spanHelp.appendChild(spanIcon);
+		spanHelp.appendChild(spanText);
+		lbl.setAttribute("for","exclChbox");
+		spanIcon.innerHTML = "?";
+		chckbox.setAttribute("type","checkbox");
+		chckbox.setAttribute("id","exclChbox");
+		chckbox.setAttribute("onclick","webpage.ReloadSection();");
+		langFlags[0] = lbl;
+		langFlags[1] = spanText;
+		spanIcon.addEventListener("mouseover",function(obj){
+			spanText.classList.add("tooltip");
+		});			
+		spanIcon.addEventListener("mouseleave",function(obj){
+			spanText.classList.remove("tooltip");
+		});
 	}
-	this.ReCreatePage = function(section){
-		pageInitializer.ReloadGroup("wholePage");
-	}
-	this.ReloadSection = function(){
-		pageInitializer.ReloadGroup("sectionChange");
-	}
-	/*Localization*/
-	this.RegisterFunciton = function(fname,fn,sdata){
-		webpage[fname] = function(ddata){return fn(ddata,sdata);};
-	}	
+	langFlags[0].innerHTML = Localization.ExclLangChck[Localization.Selected];
+	langFlags[1].innerHTML = Localization.ExclLangHelp[Localization.Selected];
 }
 
 function InitPage(){
-	window.webpage = new Page();
-	webpage.RegisterFunciton("SelectArticle",function(obj){
+	Localization.GetLanguage();
+	window.webpage = new Page();	
+	window.webpage.RegisterFunciton("SelectArticle",function(obj){
 		var articles = document.querySelectorAll(".article").forEach(function(el){
 			el.classList.add("invisible");
 		});
@@ -254,7 +170,7 @@ function InitPage(){
 		hey.querySelector(".invisible").classList.remove("invisible");
 		hey.querySelector("hr").classList.add("invisible");
 	});
-	webpage.RegisterFunciton("GetSection",function(){
+	window.webpage.RegisterFunciton("GetSection",function(){
 		if(location.hash!=""){
 			var tmp = Number.parseInt(location.hash.substr(1));
 			if(isNaN(tmp)){
@@ -266,20 +182,49 @@ function InitPage(){
 		}
 		return 1;
 	});
-	webpage.RegisterFunciton("LoadSection",function(section){
+	window.webpage.RegisterFunciton("LoadSection",function(section){
 		section = Number.parseInt(section);
 		if(!isNaN(section)){
 			if(section > 0 && section < 100){
 				location.hash = section;
 			}
 		}
-		webpage.ReloadSection();
+		window.webpage.ReloadSection();
 	});
-	webpage.RegisterFunciton("SetLanguage",function(sel){
+	window.webpage.RegisterFunciton("SetLanguage",function(sel){
 		Localization.SetLanguage(sel.selectedOptions[0].value);
-		webpage.ReCreatePage();
+		window.webpage.ReCreatePage();
 	});	
-	webpage.ReCreatePage();
+	
+	/*Creating the page types using the above functions*/
+	var WebTitle = window.webpage.CreateType(WebsiteLocalization,"WebTitle","title");					//WebTitle	
+	var WebHeadline = window.webpage.CreateType(WebsiteLocalization,"WebHeadline",".webheadline");		//WebHeadline	
+	var WebMotto = window.webpage.CreateType(WebsiteLocalization,"WebMotto",".webmotto");				//WebMotto
+	var Webflags = window.webpage.CreateType(LanguageFlags,null,".webflags");							//flags
+	var WebSection = window.webpage.CreateType(GetSections,null,".websection");						//section
+	var WebContent = window.webpage.CreateType(GetLeads,null,".webcontent");							//content
+	/*Creating the page groups*/
+	window.webpage.AddPageGroup("sectionChange",WebContent);
+	window.webpage.AddPageGroup("wholePage",WebTitle);
+	window.webpage.AddPageGroup("wholePage",WebHeadline);
+	window.webpage.AddPageGroup("wholePage",WebMotto);
+	window.webpage.AddPageGroup("wholePage",Webflags);
+	window.webpage.AddPageGroup("wholePage",WebSection);
+	window.webpage.AddPageGroup("wholePage",WebContent);
+	window.webpage.ReCreatePage = function(section){
+		window.webpage.ReloadGroup("wholePage");
+	}
+	window.webpage.ReloadSection = function(){
+		window.webpage.ReloadGroup("sectionChange");
+	}
+	window.webpage.ReCreatePage();
+	document.getElementsByClassName("partC")[0].addEventListener('DOMSubtreeModified', OnHeightChange, false);
+}
+
+function OnHeightChange(C){
+	var B = document.getElementsByClassName("partB");
+	C = $(C.target).parents(".partC")[0];
+	B[0].style = "height:"+C.scrollHeight+"px";
 }
 
 document.onload = InitPage();
--- dev/js/utils.js
+++ dev/js/utils.js
@@ -1,3 +1,47 @@
+/*Page object*/
+function Page(){
+	var pageInitializer = new PageInitializer();
+	/**	provides convenient methods because allows me to register page types in multiple groups **/
+	function PageInitializer(){
+		var types = {};
+		this.CreateType = function(fn,data,selector,multiSelect){
+			return function(){
+				fn(data,selector,multiSelect);
+			};
+		}
+		this.AddPageGroup = function(gname,pageType){
+			if(typeof types[gname] == "undefined"){
+				types[gname] = [];
+			}
+			var length = types[gname].length;
+			types[gname][length] = pageType;
+		}
+		this.ReloadGroup = function(gname){
+			types[gname].forEach(function(elem){
+				elem();
+			});
+		}
+	}
+	var that = this;
+	this.RegisterWebPart = function(idName,selector){
+		that[idName] = function(){
+			return document.querySelector(selector);
+		}
+	}
+	this.RegisterFunciton = function(fname,fn,sdata){
+		that[fname] = function(ddata){return fn(ddata,sdata);};
+	}
+	this.AddPageGroup = function(gname,pageType){
+		pageInitializer.AddPageGroup(gname,pageType);
+	}
+	this.ReloadGroup = function(gname){
+		pageInitializer.ReloadGroup(gname);
+	}
+	this.CreateType = function(fn,data,selector,multiSelect){
+		return pageInitializer.CreateType(fn,data,selector,multiSelect);
+	}
+}
+
 function CookieHandler(){
 	this.SetCookieValue = function(cname, cvalue, date) {
 		document.cookie = cname + "=" + cvalue + ";" + "expires="+date.toUTCString() + ";";
@@ -83,4 +127,16 @@ Array.prototype.getIndex = function (elem, comparator) {
 	for (var i = 0; i < this.length; i++) {
 		if (comparator(this[i], elem)) { return i; }
 	}
+}
+
+Array.prototype.toString = function (convertor, separator) {
+	separator = separator || ", ";
+	var txt = "";
+	var Len = this.length - 1;
+	var i = 0;
+	while(i < Len) {
+		txt += convertor(this[i++]) + separator;
+	}
+	txt += convertor(this[i]);
+	return txt;
 }
